436c6b591e8bda3111f6124ebce657ff0a0fc338
Yamato Matsuoka <yamaton@gmail.com>
2023-03-23 16:56:55 -0400

    Lint

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,8 @@
 import { URLExt } from '@jupyterlab/coreutils';
 import { ServerConnection } from '@jupyterlab/services';
 
+const API_NAMESPACE = 'myextension';
+
 /**
  * Call the API extension
  *
@@ -14,11 +16,7 @@ export async function requestAPI<T>(
 ): Promise<T> {
   // Make request to Jupyter API
   const settings = ServerConnection.makeSettings();
-  const requestUrl = URLExt.join(
-    settings.baseUrl,
-    'myextension', // API Namespace
-    endPoint
-  );
+  const requestUrl = URLExt.join(settings.baseUrl, API_NAMESPACE, endPoint);
 
   let response: Response;
   try {
@@ -27,19 +25,10 @@ export async function requestAPI<T>(
     throw new ServerConnection.NetworkError(error as TypeError);
   }
 
-  let data: any = await response.text();
-
-  if (data.length > 0) {
-    try {
-      data = JSON.parse(data);
-    } catch (error) {
-      console.log('Not a JSON response body.', response);
-    }
-  }
-
   if (!response.ok) {
-    throw new ServerConnection.ResponseError(response, data.message || data);
+    throw new ServerConnection.ResponseError(response, response.statusText);
   }
 
+  const data = await response.json();
   return data;
 }
